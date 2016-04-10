96f24aab4d68dd7133aa787836071df9efd7ad2b
Eli <efiliba@hotmail.com>
2016-04-10 17:17:57 +1000

    Cleaned up config and had to serve font-awesome from cdn

--- server/config.js
+++ server/config.js
@@ -1,26 +1,31 @@
 import path from 'path';
 
-const staticFilesPath = 'https://storage.googleapis.com/bestcc';
-//const staticFilesPath = '';
-
 const config = {
     port: process.env.PORT || 8000,
     //baseURL: typeof window === 'undefined' ? process.env.BASE_URL || (`http://localhost:${(process.env.PORT || 8000)}`) : '',
-    apiUrl: 'http://bcc-api.appspot.com',
-    //apiUrl: 'http://14.202.155.38',
-    //apiUrl: 'http://localhost:8001',
-    staticFiles: staticFilesPath,
-    serveStaticFiles: staticFilesPath,
-    //serveStaticFiles: path.resolve(__dirname, '../build'),
-    //avatarPath: staticFilesPath + '/images',
-    avatarPath: 'https://storage.googleapis.com/bestcc/images',
-    cssPath: staticFilesPath + '/css/' + (process.env.NODE_ENV === 'production' ? 'app.min.css' : 'app.css')
+    apiUrl: 'http://bcc-api.appspot.com',    
+    avatarPath: 'https://storage.googleapis.com/bestcc/images'
+    //avatarPath: staticFilesPath + '/images'
 };
+
+if (process.env.NODE_ENV === 'production') {
+    const staticFilesPath = 'https://storage.googleapis.com/bestcc';
+
+    config.staticFiles = staticFilesPath;
+    config.serveStaticFiles = staticFilesPath;
+    config.cssPath = staticFilesPath + '/css/app.min.css';
+} else {
+    //config.apiUrl = 'http://localhost:8001';
+    //config.apiUrl = 'http://14.202.155.38';
+    config.staticFiles = '';
+    config.serveStaticFiles = path.resolve(__dirname, '../build');
+    config.cssPath = '/css/app.css';
+}
 
 export default config;
 
 export const apiUrl = config.apiUrl;
-export const staticFiles = staticFilesPath;
+export const staticFiles = config.staticFiles;
 export const serveStaticFiles = config.serveStaticFiles;
 export const avatarPath = config.avatarPath;
 export const cssPath = config.cssPath;
--- server/template.js
+++ server/template.js
@@ -10,11 +10,10 @@ export default (html, initialState) => `
         <title>Best Choice Care</title>
 
 		<link rel="stylesheet" href="${staticFiles}/css/bootstrap-4.0-flex.css"/>
-		<link rel="stylesheet" href="${staticFiles}/css/font-awesome-4.5.0.min.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" href="${cssPath}"/>
         <link rel="shortcut icon" href="${staticFiles}/images/fav_icon.jpg" type="image/jpg"/>
-        <link rel="stylesheet" href="${staticFiles}/css/animate.min.css"/>
-       
+        <link rel="stylesheet" href="${staticFiles}/css/animate.min.css"/>       
       </head>
       <body>
         <div id="root">${html}</div>
@@ -25,7 +24,8 @@ export default (html, initialState) => `
    		<script src="${staticFiles}/scripts/cbpAnimatedHeader.min.js"></script>
    		<script src="${staticFiles}/scripts/jquery-2.1.4.min.js"></script>
 		<script src="${staticFiles}/scripts/tether.min.js"></script><!-- Tether for Bootstrap -->
-		<script src="${staticFiles}/scripts/bootstrap-4.0.0-alpha.2.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+		<script src="${staticFiles}/scripts/bootstrap-4.0.0-alpha.2.min.js"></script>
+		<!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script-->
         <script src="${staticFiles}/dist/bundle.js"></script>
         <script src="${staticFiles}/scripts/jquery.easing.1.3.js"></script>
         <script src="${staticFiles}/scripts/jquery.stellar.min.js"></script>
