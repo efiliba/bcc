c0372e04737422fa01d6ffc66b39763e455b27d5
Eli <efiliba@hotmail.com>
2016-04-14 23:25:32 +1000

    Moved config file. Added 'Features Under Development' and linksfor demonstrations

---
+++ src/components/Site/HeaderContainer.js
@@ -0,0 +1,35 @@
+﻿import {connect} from 'react-redux';
+import Header from './Header';
+
+const mapStateToProps = (state) => ({
+    links: [{
+        label: "Home",
+        link: '/'
+    }, {
+        label: "Carers",
+        link: '/carers'
+    }, {
+        label: "Register",
+        link: '/register'
+    }, {        
+        label: "Top",
+    }, {
+        label: "About Us",
+        href: "#about" 
+    }, {
+        label: "Services",
+        href: "#services" 
+    }, {
+        label: "Contact Us",
+        href: "#contactUsForm" 
+    }]
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onSubmit: (data) => {
+        dispatch(null);
+    }
+});
+
+//export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(mapStateToProps)(Header);
